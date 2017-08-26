e02999d1a3b84ebcb77a6900077248538d37c272
rafael urrutia <rafael.urrutia@fusiona.cl>
2017-08-26 13:24:33 -0300

    Terminado

---
+++ 1.1 Asesoría Personalizada 2.1.php
@@ -0,0 +1,164 @@
+<?php include "header.php" ?>
+    <div id="content" class="wrapper">
+        <div class="heading-banner bg-cover col-xs-12 col-md-12" style="background-image: url(img/img-inversiones.png)">
+            <div class="caption">
+                <div class="v-center">
+                    <div>
+                        <h1>Administración de inversión</h1>
+                        <p>El mundo de las inversiones está lleno de oportunidades y saber identificarlas es una habilidad que el equipo de Banchile Inversiones pone a su disposición y se compromete a entregar.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="tab-container" class="tab-container">
+            <ul class='etabs'>
+                <li class='tab col-sm-3 col-md-3'><a href="#tab1">ASESORÍA PERSONALIZADA</a></li>
+                <li class='tab col-sm-3 col-md-3'><a href="#tab2">PRODUCTOS DE INVERSIÓN</a></li>
+                <li class='tab col-sm-3 col-md-3'><a href="#tab3">ESTUDIOS BANCHILE</a></li>
+                <li class='tab col-sm-3 col-md-3'><a href="#tab4">PRIVATE WEALTH MANAGEMENT</a></li>
+            </ul>
+            <div id="tab1" class="tab-content">
+                <div class="layout-3 layout">
+                    <div class="inner">
+                        <div class="col-md-6">
+                            <h2>Asesoría personalizada</h2>
+                            
+                            <br> <br>
+                            
+                            <p>Con el conocimiento, solidez y experiencia de nuestros especialistas, en Banca Privada de Banco de Chile, en conjunto con Banchile Inversiones, aportamos íntegramente en la atención de sus necesidades financieras, poniendo a su disposición oportunidades de inversión amplias y diferenciadoras.
+                            </p>
+
+                            <p>
+                                Entendemos que la gestión de su patrimonio requiere de una alta dedicación, por lo que nuestro servicio se basa en la entrega de respuestas rápidas, confiables y especializadas a través de asesoría en inversiones a nivel internacional.
+                            </p>
+
+                            <p>
+                                Le invitamos a conocer un completo mundo de servicios exclusivos, otorgados por los mejores expertos de la Banca Privada de Banco de Chile y de Banchile Inversiones.
+                            </p>
+
+                        </div>
+                        <div class="col-md-6">
+                            <img src="img/img-inversiones-1.png">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div id="tab2" class="tab-content">
+                <div class="layout-2 layout">
+                    <div class="heading style-2">
+                        <h2>Productos de Inversión</h2>
+                        <p>A través de Banchile Inversiones, en Banca Privada de Banco de Chile le ofrecemos servicios y soluciones con una visión acorde a su perfil y horizonte de inversión, asesorándolo en sus decisiones y en la administración financiera de su cartera, como también la de su grupo familiar. Acceda a la más amplia variedad de productos de inversión, tales como:</p>
+                    </div>
+                    <div class="inner">
+                        <img src="img/img-inversiones-2.png">
+                        <ul class="col-md-6 list-style-2">
+                            <li>
+                                Fondos mutuos
+                            </li>
+                            <li>
+                                Acciones
+                            </li>
+                            <li>
+                                Moneda extranjera
+                            </li>
+                            <li>
+                                Ahorro Previsional Voluntario (APV)
+                            </li>
+                            <li>
+                                Renta fija
+                            </li>
+                        </ul>
+
+                        <ul class="col-md-6 list-style-2">
+
+                            <li>
+                                Cartera Activa
+                            </li>
+                            <li>
+                                Fondos de Inversión
+                            </li>
+                            <li>
+                                Mercado de valores extranjeros
+                            </li>
+                            <li>
+                                Productos derivados
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div id="tab3" class="tab-content">
+                <div class="layout-3 layout">
+                    <div class="inner">
+                        <div class="col-md-6">
+                            <h2>Estudios Banchile</h2>
+
+                            <br> <br>
+
+                            <p>Con el amplio conocimiento del departamento de Estudios de Banchile Inversiones, le entregaremos la información necesaria y oportuna para apoyarle en la toma de decisiones. 
+                            </p>
+
+                            <p>
+                                Este departamento cuenta con reconocida experiencia, elaborando profundos análisis de la coyuntura económica y de los mercados, para entregar el respaldo necesario a la hora de invertir. Banchile Inversiones posee una de las coberturas de empresas más amplias en el país, abarcando las compañías listadas más relevantes en Chile, junto con una visión del mercado local de renta fija y de las principales variables económicas nacionales e internacionales.
+                            </p>
+
+                        </div>
+                        <div class="col-md-6">
+                            <img src="img/img-inversiones-3.png">
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div id="tab4" class="tab-content">
+                <div class="layout-2 layout">
+                    <div class="heading style-2">
+                        <h2>Private Wealth Management</h2>
+                        <p>Entendiendo que sus inversiones requieren de la mayor atención y agilidad, desarrollamos Private Wealth Management, la mejor Banca Privada de Chile, a través de la cual una dupla de expertos de Banco de Chile y Banchile Inversiones gestiona de manera integrada sus necesidades financieras. 
+                        </p>
+                    </div>
+                    <div class="inner">
+                        <img src="img/img-inversiones-4.png">
+
+                        <div class="col-md-12">
+                            <br> <br>
+
+                            <p class="text-center">
+                                Por medio de este exclusivo modelo de atención, nuestros clientes pueden acceder a productos y servicios de alta personalización, tales como:
+                            </p>
+
+                            <ul class="col-xs-12 col-md-12 list-style-3">
+                                <li>Asesoría global en inversiones con arquitectura abierta.</li>
+                                <li>Productos y servicios bancarios de acuerdo a sus necesidades.</li>
+                                <li>Estructura financiera de acuerdo con su perfil, financiamiento de negocios de inversiones.</li>
+                                <li>Acceso a servicios de finanzas corporativas.</li>
+                            </ul>
+
+                            <p class="text-center">
+                                Adicionalmente, en el ámbito de la gestión de su patrimonio, nuestros clientes disponen de servicios de especialidad, como:
+                            </p>
+
+                            <ul class="col-md-12 list-style-3">
+                                <li>Comité de inversiones.</li>
+                                <li>Departamento de Estudios.</li>
+                                <li>Mesa de transacciones dedicada.</li>
+                                <li>Intermediación de monedas e instrumentos de deuda y capitalización.</li>
+                                <li>Productos de inversión locales e internacionales, entre otros.</li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+
+
+    <?php include "footer.php" ?>
